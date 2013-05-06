35674edc2f6e8a10ab53edc9541559ec0388177b
Brian Lai <brian.lai.ym@gmail.com>
2013-05-06 16:45:03 -0700

    use foundation 4.1.6 (stylus version)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var path = require('path');
-
-var folderMount = function folderMount(connect, point) {
-  return connect.static(path.resolve(point));
-};
-
 exports = module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-stylus');
 
   // Project configuration.
@@ -30,7 +23,7 @@ exports = module.exports = function(grunt) {
           sassDir: 'sass',
           cssDir: 'dist/scss',
           outputStyle: 'expanded',
-          require: 'zurb-foundation'
+          debugInfo: false
         }
       }
     },
@@ -42,7 +35,7 @@ exports = module.exports = function(grunt) {
           // paths: ['path/to/import', 'another/to/import'],
           // urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
           use: [
-            require('zurb-foundation') // use stylus plugin at compile time
+            require('foundation') // use stylus plugin at compile time
           ]
         },
         files: {
@@ -53,27 +46,9 @@ exports = module.exports = function(grunt) {
 
     clean: {
       build: ['dist']
-    },
-
-    connect: {
-      server: {
-        options: {
-          port: 3000,
-          middleware: function(connect) {
-            return [
-              folderMount(connect, 'public'),
-              folderMount(connect, 'dist')
-            ];
-          }
-        }
-      }
     }
   });
 
-  grunt.registerTask('async', function() {
-    this.async();
-  });
-
   // Default task.
-  grunt.registerTask('default', ['clean', 'stylus', 'compass', 'connect', 'async']);
+  grunt.registerTask('default', ['clean', 'stylus', 'compass']);
 };
